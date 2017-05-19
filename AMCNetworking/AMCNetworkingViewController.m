3723474a2792215d3096cc3f1438ea7ec41c17dd
cjlcooper <cjlcooper@163.com>
2017-05-19 14:45:38 +0800

    init

---
+++ AMCNetworking/AMCNetworkingViewController.m
@@ -0,0 +1,30 @@
+//
+//  AMCNetworkingViewController.m
+//  AMCNetworking
+//
+//  Created by dynamsoft on 19/05/2017.
+//  Copyright © 2017 dynamsoft. All rights reserved.
+//
+
+#import "AMCNetworkingViewController.h"
+#import <CFNetwork/CFNetwork.h>
+
+@interface AMCNetworkingViewController ()
+
+@end
+
+@implementation AMCNetworkingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+@end
